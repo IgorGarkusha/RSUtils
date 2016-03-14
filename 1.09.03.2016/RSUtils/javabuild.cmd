3828b5ae55a91bf01b2dacc0b051f1a470056b66
Igor Garkusha <igor_garik@ua.fm>
2016-03-14 10:23:58 +0200

    Update javabuild.cmd

--- 1.09.03.2016/RSUtils/javabuild.cmd
+++ 1.09.03.2016/RSUtils/javabuild.cmd
@@ -54,14 +54,14 @@ echo CREATE START SCRIPTS...
 echo.
 
 echo @echo off >  binary\target\modis_downloader.cmd
-echo java -cp %RSUTILS_HOME%\librsutils_downloader.jar;%RSUTILS_HOME%\modis_downloader.jar;. modis_downloader.modis_download %%1 >> binary\target\modis_downloader.cmd
+echo java -cp %RSUTILS_HOME%\librsutils_downloader.jar;%RSUTILS_HOME%\modis_downloader.jar;. modis_downloader.modis_download %%* >> binary\target\modis_downloader.cmd
 echo. >> binary\target\modis_downloader.cmd
 
 echo @echo off > binary\target\s1a_grd_downloader.cmd
-echo java -cp %RSUTILS_HOME%\librsutils_downloader.jar;%RSUTILS_HOME%\s1a_grd_downloader.jar;. s1a_grd_downloader.s1a_grd_download %%1 >> binary\target\s1a_grd_downloader.cmd
+echo java -cp %RSUTILS_HOME%\librsutils_downloader.jar;%RSUTILS_HOME%\s1a_grd_downloader.jar;. s1a_grd_downloader.s1a_grd_download %%* >> binary\target\s1a_grd_downloader.cmd
 echo. >> binary\target\s1a_grd_downloader.cmd
 
 echo @echo off > binary\target\s2a_tile_downloader.cmd
-echo java -cp %RSUTILS_HOME%\librsutils_downloader.jar;%RSUTILS_HOME%\s2a_tile_downloader.jar;. s2a_tile_downloader.s2_tile_download %%1 >> binary\target\s2a_tile_downloader.cmd
+echo java -cp %RSUTILS_HOME%\librsutils_downloader.jar;%RSUTILS_HOME%\s2a_tile_downloader.jar;. s2a_tile_downloader.s2_tile_download %%* >> binary\target\s2a_tile_downloader.cmd
 echo. >> binary\target\s2a_tile_downloader.cmd
 
