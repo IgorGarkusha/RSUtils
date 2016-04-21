dc874a8f4d055f4f4fd2cbb4d0b517456db88971
Igor Garkusha <igor_garik@ua.fm>
2016-04-21 15:08:12 +0300

    Update s2_tile_download.java

--- src/downloaders/sentinel/s2a_tile_downloader/src/s2a_tile_downloader/s2_tile_download.java
+++ src/downloaders/sentinel/s2a_tile_downloader/src/s2a_tile_downloader/s2_tile_download.java
@@ -27,7 +27,7 @@ import javax.swing.*;
 public class s2_tile_download
 {
 	public static final String PROG_VERSION = "1";
-	public static final String DATE_VERSION = "09.03.2016";
+	public static final String DATE_VERSION = "21.04.2016";
 	
 	public static void main(String[] args)
 	{
