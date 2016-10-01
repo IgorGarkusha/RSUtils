8adb69c49c5ca1974f8f52c9cc1b58d54115501b
Igor Garkusha <igor_garik@ua.fm>
2016-10-01 07:39:07 +0300

    Add files via upload

--- external/create_stack.cmd
+++ external/create_stack.cmd
@@ -1,1 +1,10 @@
-
+@echo off
+set PYTHONPATH=%PYTHONPATH%;%QGIS_PYTHON_HOME%\Lib\site-packages\osgeo
+if -%1 == - goto error
+if -%2 == - goto error
+if -%3 == - goto error
+%QGIS_HOME%\bin\gdal_merge.py -separate -o %*
+goto exit
+:error
+echo Example: create_stack.cmd OutputMultiBandImage.tif Band1.tif Band2.tif ... BandN.tif
+:exit
