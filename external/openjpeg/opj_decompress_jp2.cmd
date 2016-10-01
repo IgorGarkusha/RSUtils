0e2eacf7ba4dec45c3882f0538db54a730345d90
Igor Garkusha <igor_garik@ua.fm>
2016-10-01 07:40:23 +0300

    Add files via upload

--- external/openjpeg/opj_decompress_jp2.cmd
+++ external/openjpeg/opj_decompress_jp2.cmd
@@ -1,1 +1,10 @@
-
+@echo off
+set PATH=%PATH%;%RSUTILS_HOME%\lib
+if -%1 == - goto error
+if -%2 == - goto error
+"%RSUTILS_HOME%"\opj_decompress.exe -i %1 -o %2
+goto exit
+:error
+"%RSUTILS_HOME%"\opj_decompress
+echo Example: %RSUTILS_HOME%\opj_decompress_jp2 input.jp2 output.tif
+:exit
